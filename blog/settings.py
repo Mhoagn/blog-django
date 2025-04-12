c3afea5b1ebf7316f9c871760ea142dd275a079f
MinhHoang <minhhoangdayyyy07@gmail.com>
2025-04-12 16:16:55 +0700

    connect with postgresql

--- blog/settings.py
+++ blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_project',
+        'USER': 'postgres',  
+        'PASSWORD': 'mh071204',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
