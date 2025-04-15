6676a0db00466cf5b9d27b95667690f0c2c8cf1b
MinhHoang <minhhoangdayyyy07@gmail.com>
2025-04-15 19:12:37 +0700

    solving conflict

--- blog/urls.py
+++ blog/urls.py
@@ -15,14 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('homepage.urls')),
-    path('blog/',include('blogapp.urls')),
-<<<<<<< HEAD
-    path('user/',include('userapp.urls'))
-=======
->>>>>>> feature/homepage-fe
+    path('', include('homepage.urls')),
+    path('blog/', include('blogapp.urls')),
+    path('user/', include('userapp.urls'))
 ]
